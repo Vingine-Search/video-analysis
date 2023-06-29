89dea16c2ef7f822ce7640caa079cb1e8ce2acb3
BigFish2086 <a8686.ibrahim@gmail.com>
2023-06-29 16:02:48 +0300

    add running_avg.py

---
+++ train/running_avg.py
@@ -0,0 +1,27 @@
+# this class is used to store the running average & the running sum 
+# and the current value of a variable
+
+class RunningAvg(object):
+    """Computes and stores the average, sum and current value"""
+
+    def __init__(self, name, fmt=':f'):
+        self.name = name
+        self.fmt = fmt
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+    def __str__(self):
+        fmtstr = '{name} {val' + self.fmt + '} ({avg' + self.fmt + '})'
+        return fmtstr.format(**self.__dict__)
+
