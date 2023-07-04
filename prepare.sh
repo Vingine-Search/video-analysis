352dfbc547ce8cae2df9daaadd05ae0bf22c4330
BigFish2086 <a8686.ibrahim@gmail.com>
2023-07-04 14:29:57 +0300

    update prepare.sh

--- prepare.sh
+++ prepare.sh
@@ -10,15 +10,27 @@ weights_dir="./weights"
 echo "[*] Downloading VRD dataset"
 mkdir -p $json_dir
 wget https://cs.stanford.edu/people/ranjaykrishna/vrd/json_dataset.zip -P $json_dir
+# check if unzip is installed
+if ! [ -x "$(command -v unzip)" ]; then
+  echo '[-] Error: unzip is not installed.' >&2
+  exit 1
+fi
 unzip $json_dir/json_dataset.zip -d $json_dir
-
-echo "[*] Downloading word2vectors"
-# https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/view?usp=drive_link&resourcekey=0-wjGZdNAUop6WykTtMip30g
-mkdir -p $wordvec_dir
-gdown https://drive.google.com/uc?id=0B7XkCwpI5KDYNlNUTTlSS21pQmM -O $wordvec_dir/GoogleNews-vectors-negative300.bin.gz 
 
 echo "[*] Downloading weights"
 mkdir -p $weights_dir
 # https://drive.google.com/file/d/1UJXqYKEx9BvsYNvksQQQlaUcTQr_MhDb/view
 gdown https://drive.google.com/uc?id=1UJXqYKEx9BvsYNvksQQQlaUcTQr_MhDb -O $weights_dir/S3D_kinetics400.pt
+
+echo "[*] Downloading word2vectors"
+# https://drive.google.com/file/d/0B7XkCwpI5KDYNlNUTTlSS21pQmM/view?usp=drive_link&resourcekey=0-wjGZdNAUop6WykTtMip30g
+mkdir -p $wordvec_dir
+gdown https://drive.google.com/uc?id=0B7XkCwpI5KDYNlNUTTlSS21pQmM -O $wordvec_dir/GoogleNews-vectors-negative300.bin.gz 
+# check if gunzip is installed
+if ! [ -x "$(command -v gunzip)" ]; then
+  echo '[-] Error: gunzip is not installed.' >&2
+  exit 1
+fi
+gunzip $wordvec_dir/GoogleNews-vectors-negative300.bin.gz
+
 
